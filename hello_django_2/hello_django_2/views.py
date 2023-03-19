200a8cf38a90841ff88de37a891144658d142cd0
klen1980 <nick300880@gmail.com>
2023-03-19 17:16:54 +0300

    final

--- hello_django_2/hello_django_2/views.py
+++ hello_django_2/hello_django_2/views.py
@@ -5,5 +5,10 @@ def about(request):
     a = 5+6
     return render(request, 'about.html', {'greting': a})
 
+def reverse (request):
+    usertext = request.GET['username']
+    reverced= usertext[::-1]
+    return render(request, 'reverse.html', {'word': reverced})
+
 def home (request):
     return HttpResponse ('This is my homepage')
